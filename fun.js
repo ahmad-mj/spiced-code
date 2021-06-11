f115814e59a0ab010d565cf9c6671ac0018b1c75
Ahmad AlMujawer <ahmad_almujawer@icloud.com>
2021-06-11 13:47:47 +0200

    f

---
+++ fun.js
@@ -0,0 +1,16 @@
+var x;
+var doubleX = 1000
+function timesTwo(5) {
+
+}
+timesTwo(x) = doubleX;
+
+var numbers = ["x", "doubleX"] 
+for (var i =0; i<numbers.length; i++){
+    console.log(x, doublex);
+}
+
+numbers = {
+    y: doubleX,
+}
+
