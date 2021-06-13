d97a54bb6416ede8c7472c5be31aa000c5f92182
Ahmad AlMujawer <ahmad_almujawer@icloud.com>
2021-06-13 17:55:52 +0200

    added ex. 2 and 3

--- week1/day3/day3.js
+++ week1/day3/day3.js
@@ -14,3 +14,27 @@ console.log(fnSum(5, 10)); //15
 console.log(fnSum(5, 10, 15)); //30;
 
 console.log(fnSum(5, 10, 15, 100, 200)); //330
+
+//Exercise2
+function waitThenRun(callback) {
+    setTimeout(callback, 1500);
+}
+waitThenRun(function () {
+    console.log("Hello!");
+    waitThenRun(function () {
+        console.log("Goodbye!");
+    }); // logs 'Goodbye!' 1.5 seconds later
+}); // logs 'Hello!' 1.5 seconds later
+
+//Exercise3
+function myFun(num) {
+    if (num >= 1) {
+        while (num < 1000000) {
+            num *= 10;
+        }
+    } else {
+        num = "ERROR";
+    }
+    return num;
+}
+myFun();
