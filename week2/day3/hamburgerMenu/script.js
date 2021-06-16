df4fcfe419367e187f4998daa59242bdd5f7a2d6
Ahmad AlMujawer <ahmad_almujawer@icloud.com>
2021-06-16 18:25:59 +0200

    hamburger menu

--- week2/day3/hamburgerMenu/script.js
+++ week2/day3/hamburgerMenu/script.js
@@ -1,15 +1,28 @@
 (function () {
     var overlay = document.getElementById("overlayMenu");
+    var sideNav = document.getElementById("sideNavMenu");
     var openMenu = document.getElementById("menu");
     var closeMenu = document.getElementById("x_menu");
-
-    openMenu.addEventListener("click", function () {
+    var hideMenu = document.getElementById("hideMenu");
+    console.log(hideMenu);
+    openMenu.addEventListener("click", function (e) {
         console.log("clicked on menu");
         overlay.classList.add("menu_js");
+        sideNav.classList.add("menu_js");
+        e.stopPropagation();
     });
 
-    closeMenu.addEventListener("click", function () {
+    closeMenu.addEventListener("click", function (e) {
         console.log("clicked on x");
         overlay.classList.remove("menu_js");
+        sideNav.classList.remove("menu_js");
+        e.stopPropagation();
+    });
+
+    hideMenu.addEventListener("click", function (e) {
+        console.log("hi i clicked in the body");
+        overlay.classList.remove("menu_js");
+        sideNav.classList.remove("menu_js");
+        e.stopPropagation();
     });
 })();
