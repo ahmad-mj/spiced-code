55d5d075764525bf764592a82066409f67eb2a03
Ahmad AlMujawer <ahmad_almujawer@icloud.com>
2021-06-24 20:16:27 +0200

    ajax--I can not log what i added to the html _linksStr_ ticker is not working

--- week3/day4/Ajax_ticker/ticker.js
+++ week3/day4/Ajax_ticker/ticker.js
@@ -1,62 +1,76 @@
-$.ajax({
-    url: "/data.json",
-    method: "GET",
+(function () {
+    // var headlines = document.getElementById("headlines");
+    var jQheadlines = $("#headlines");
+    console.log("jQuery headlines", jQheadlines);
 
-    success: function (data) {
-        console.log("data: ", data);
-        var htmlStr = "";
-        for (var i = 0; i < data.length; i++) {
-            console.log("data.text[i]: ", data[i]);
-            htmlStr += "<a>" + data[i] + "</a>";
-        }
-        console.log("htmlStr: ", htmlStr);
-        $("#headlines").html(htmlStr);
+    // var left = headlines.offsetLeft;
+    var jQleft = jQheadlines.offset().left;
+    // var links = document.getElementsByTagName("a");
+    var jQlinks = $("a");
 
-        (function () {
-            var jQheadlines = $("#headlines");
-            // console.log("jQuery headlines", jQheadlines);
-            var jQleft = jQheadlines.offset().left;
-            var jQlinks = $("a");
-            var myReqId;
+    var myReqId;
+    $.ajax({
+        url: "/data.json",
+        method: "GET",
 
-            for (var i = 0; i < jQlinks.length; i++) {
-                jQlinks.eq(i).on("mouseenter", function (evt) {
-                    cancelAnimationFrame(myReqId);
-                    console.log("mosueenred on an <a>");
-                    console.log("moused on this element:", evt.target);
-                    $(evt.target).css({
-                        color: "red",
-                    });
-                });
-
-                jQlinks.eq(i).on("mouseleave", function (evt) {
-                    console.log("you just mouseleft on an a tag");
-                    console.log("you mouse left this element:", evt.target);
-                    $(evt.target).css({
-                        color: "green",
-                    });
-
-                    moveHeadlines();
-                });
+        success: function (data) {
+            console.log("data: ", data);
+            var linksStr = "";
+            for (var i = 0; i < data[i].length; i++) {
+                linksStr +=
+                    "<a href = " + data[i].href + ">" + data[i].text + "</a>";
+                // "<a href = " + data[i].href + ">" + data[i].text + "</a>";
             }
+            console.log("linksStr: ", linksStr);
 
-            function moveHeadlines() {
-                jQleft--;
-                if (jQleft <= -jQlinks.eq(0).Width) {
-                    jQleft += jQlinks.eq(0).Width;
-                    jQheadlines.appendChild(jQlinks.eq(0));
-                }
-                jQheadlines.css({
-                    left: jQleft + "px",
-                });
+            console.log("data[i].text: ", data[i].text);
+            console.log("data[i].href: ", data[i].href);
+            jQheadlines.html(linksStr);
+        },
+        // error: function (error) {
+        //     console.log("error: ", error);
+        // },
+    });
 
-                myReqId = requestAnimationFrame(moveHeadlines);
-            }
+    for (var i = 0; i < jQlinks.length; i++) {
+        jQlinks.eq(i).on("mouseenter", function (evt) {
+            cancelAnimationFrame(myReqId);
+            console.log("mosueenred on an <a>");
+            // console.log("moused on this element:", evt.target);
+
+            // event.target.style.color = "red";
+            $(evt.target).css({
+                color: "red",
+            });
+        });
+
+        jQlinks.eq(i).on("mouseleave", function (evt) {
+            console.log("you just mouseleft on an a tag");
+            // console.log("you mouse left this element:", evt.target);
+            // event.target.style.color = "green";
+            $(evt.target).css({
+                color: "green",
+            });
 
             moveHeadlines();
-        })();
-    },
-    error: function (error) {
-        console.log("error: ", error);
-    },
-});
+        });
+    }
+
+    function moveHeadlines() {
+        jQleft--;
+
+        if (jQleft <= -jQlinks.eq(0).Width) {
+            jQleft += jQlinks.eq(0).Width;
+            jQheadlines.appendChild(jQlinks.eq(0));
+        }
+
+        jQheadlines.css({
+            left: jQleft + "px",
+        });
+
+        myReqId = requestAnimationFrame(moveHeadlines);
+    }
+
+    moveHeadlines();
+})();
+//
