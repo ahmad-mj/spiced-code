974cf633a135a82a1ccdb9481f723c572aaa745b
Ahmad AlMujawer <ahmad_almujawer@icloud.com>
2021-06-29 09:42:38 +0200

    ihave 2bugs 1more_btn , 2no results found

--- week4/day1/spotify API/script.js
+++ week4/day1/spotify API/script.js
@@ -65,17 +65,92 @@
                         "spicedify.herokuapp.com/spotify"
                     );
                 var moreButton = $("#more_btn ");
-                if (response.next === null) {
-                    moreButton.css({ visibility: "hidden" });
-                    // console.log("moreButton should not be visible", moreButton);
-                } else if (response.next !== null) {
-                    moreButton.css({ visibility: "visible" });
-                    // console.log("moreButton should be visible", moreButton);
+
+                if (response.next) {
+                    nextUrl =
+                        response.next &&
+                        response.next.replace(
+                            "api.spotify.com/v1/search",
+                            "spicedify.herokuapp.com/spotify"
+                        );
+
                     moreButton.click(function () {
                         console.log("clicking");
+                        $.ajax({
+                            url: nextUrl,
+                            method: "GET",
+                            data: {
+                                query: userInput,
+                                type: albumOrArtist,
+                            },
+                            success: function (response) {
+                                response = response.artists || response.albums;
+                                console.log("response: ", response);
+                                var myHtml = ""; // = "" so it does not show me undefinde
+                                for (
+                                    var i = 0;
+                                    i < response.items.length;
+                                    i++
+                                ) {
+                                    // console.log(response.items[i].name);
+
+                                    var imageUrl = "/default.jpg";
+                                    if (response.items[i].images[0]) {
+                                        imageUrl =
+                                            response.items[i].images[0].url;
+                                    }
+
+                                    myHtml +=
+                                        "<a target='_blank' href=" +
+                                        response.items[i].external_urls
+                                            .spotify +
+                                        ">" +
+                                        "<img src = " +
+                                        imageUrl +
+                                        " >" +
+                                        "<p> " +
+                                        response.items[i].name +
+                                        "</p>" +
+                                        "</a>";
+                                    // there is a bug No results found for is not showing!!
+                                    var resultFor = "";
+
+                                    if (response.items.length > 0) {
+                                        resultFor +=
+                                            '<h2>Results for: "' +
+                                            $("input").val() +
+                                            '"</h2>';
+                                    } else if (response.items.length >= null) {
+                                        resultFor +=
+                                            '<h2>No results found for: "' +
+                                            $("input").val() +
+                                            '"</h2>';
+                                    }
+                                    // console.log("no result", resultFor);
+                                }
+                                $("#resultsForText").html(resultFor);
+                                // console.log("myHtml:", myHtml);
+                                $("#results-container").html(myHtml);
+                                // console.log("orginal url: ", response.next);
+
+                                var nextUrl =
+                                    response.next &&
+                                    response.next.replace(
+                                        "api.spotify.com/v1/search",
+                                        "spicedify.herokuapp.com/spotify"
+                                    );
+                            },
+                        });
                     });
-                }
-                // console.log("new url: ", nextUrl);
+                    var moreButton = $("#more_btn ");
+                    if (response.next === null) {
+                        moreButton.css({ visibility: "hidden" });
+                        // console.log("moreButton should not be visible", moreButton);
+                    } else if (response.next !== null) {
+                        moreButton.css({ visibility: "visible" });
+                        // moreButton.css({ visibility: "visible" });
+                    }
+                } //else moreButton.css({ visibility: "hidden" }); // maybe i schould add visibilty:hidden here??
             },
         });
     });
