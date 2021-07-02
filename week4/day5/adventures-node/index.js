9243919bfd5f6327bebb4acee519b089928f8c6d
Ahmad AlMujawer <ahmad_almujawer@icloud.com>
2021-07-02 05:51:41 +0200

    added text adventure excersise

---
+++ week4/day5/adventures-node/index.js
@@ -0,0 +1,45 @@
+const readline = require("readline");
+const chalk = require("chalk");
+console.log(chalk.bgCyan.red("Hello Ahmad"));
+
+const gameInterface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+const story = {
+    question: "Welcome to The Land Of Wizards! Would you like to play?",
+    answers: {
+        yes: {
+            question:
+                "You are alone in a dark forest and facing a fork in the road. Which direction do you turn?",
+            answers: {
+                left: {
+                    question:
+                        "There's a scary wizard! He asks you a tough question. What's 1+1?",
+                    answers: {
+                        2: "congratulations!",
+                    },
+                },
+                right: "This was not the right choice. Goodbye!",
+            },
+        },
+        no: "Alright then. Enjoy your day!",
+    },
+};
+
+function playGame(story) {
+    gameInterface.question(story.question, (answer) => {
+        if (!story.answers[answer]) {
+            console.log(chalk.bgRed.white("Please Cooperate!"));
+            return playGame(story);
+        } else {
+            if (typeof story.answers[answer] === "string") {
+                console.log(chalk.bgYellow.blue(story.answers[answer]));
+                gameInterface.close();
+            } else playGame(story.answers[answer]);
+        }
+    });
+}
+
+playGame(story);
