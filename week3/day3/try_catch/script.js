e94bc36c61f7d48c2f731e9f4af11e3b2ad33002
Ahmad AlMujawer <ahmad_almujawer@icloud.com>
2021-07-03 21:16:48 +0200

    modified code according to Melre comment

--- week3/day3/try_catch/script.js
+++ week3/day3/try_catch/script.js
@@ -1,15 +1,14 @@
 (function () {
     var textarea = $("textarea");
     var button = $("button");
-
     button.click(function () {
         try {
             JSON.parse(textarea.val());
+            alert("JSON is valid🤓");
         } catch (e) {
             alert("what you wrote is NOT the SON of J😞");
         }
-        JSON.parse(textarea.val());
-        alert("JSON is valid🤓");
     });
 })();
+
 // {"test":"test"}
