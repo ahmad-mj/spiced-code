4b7f998a66dc24c725388f9c948bb13ea85542de
Ahmad AlMujawer <ahmad_almujawer@icloud.com>
2021-07-08 17:12:36 +0200

    added express project

--- week5/expr-proj/index.js
+++ week5/expr-proj/index.js
@@ -1,22 +1,38 @@
 const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
-const baicAuth = require("basic-auth");
+const basicAuth = require("basic-auth");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use((req, res, next) => {
     console.log("my own middleware is running");
-    if (req.cookies.authenticated || req.url == "/cookie") {
+    console.log("req.cookies.accept ", typeof req.cookies.accept);
+    if (req.cookies.accept || req.url == "/cookie") {
+        // console.log("inside my middelware");
         next();
     } else {
+        console.log("inside my middelware");
+
         res.cookie("url", req.url);
         res.redirect("/cookie");
     }
 });
+app.use("/connectfour", (req, res, next) => {
+    var creds = basicAuth(req);
+    if (!creds || creds.name != "ahmad" || creds.pass != "connect4") {
+        res.setHeader(
+            "WWW-Authenticate",
+            'Basic realm="Enter your credentials to see this stuff."'
+        );
+        res.sendStatus(401);
+    } else {
+        next();
+    }
+});
 
-app.use(express.static("/projects"));
+app.use(express.static(`${__dirname}/projects`));
 
 app.get("/cookie", (req, res) => {
     if (req.cookies.accept) {
@@ -27,9 +43,11 @@ app.get("/cookie", (req, res) => {
 });
 
 app.post("/cookie", (req, res) => {
+    console.log("req.body", req.body);
     if (req.body.input == "accept") {
-        res.cookie("accept");
+        res.cookie("accept", true);
         let redirectionLink = req.cookies.url || "/";
+        console.log("redirectionLink", redirectionLink);
         res.redirect(redirectionLink);
     } else {
         res.send(`You should accept to see the content`);
