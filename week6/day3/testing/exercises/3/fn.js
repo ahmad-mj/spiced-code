87899aaa65007d7dfbc89906b64ea6b5eeca7fac
Ahmad AlMujawer <ahmad_almujawer@icloud.com>
2021-07-15 09:33:05 +0200

    added fn.js excerises

--- week6/day3/testing/exercises/3/fn.js
+++ week6/day3/testing/exercises/3/fn.js
@@ -1,7 +1,12 @@
 module.exports = function fn(argument) {
     if (typeof argument === "string") {
-        return argument === "";
-    } else {
-        fn(argument);
+        return argument.split("").reverse().join("");
+    } else if (Array.isArray(arguments[0])) {
+        let arr = [];
+        for (let i = 0; i < arguments[0].length; i++) {
+            arr.push(fn(arguments[0][i]));
+        }
+        return arr;
     }
+    return null;
 };
